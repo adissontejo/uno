99ef6e0105ab0ed21beeeaac0ee431fd62feba39
adissontejo <adissonmprtejo@gmail.com>
2022-12-04 15:07:10 -0300

    Add function buyCards

--- src/cards.c
+++ src/cards.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int qtCards;
 char** cards;
@@ -9,7 +11,7 @@ Vanessa
 Função: ler cartas iniciais do jogo
 
 Exemplo de entrada (scanf):
-CARDS [ A♥ A♥ A♥ A♥ A♥ A♥ A♥ ]
+HAND [ A♥ A♥ A♥ A♥ A♥ A♥ A♥ ]
 
 Tratar colcehetes, alocar memória em cards, incrementar qtCards.
 */
@@ -17,46 +19,25 @@ void readCards() {
 
 }
 
-/*
-Ádisson
-
-Função: comprar cartas durante o jogo
-
-Exemplo de comando (printf):
-BUY 4
-
-Exemplo de resposta (scanf):
-A♥
-A♥
-A♥
-A♥
-
-Realocar cards, adicionar cartas novas na lista, incrementar qtCards
-*/
 void buyCards(int qt) {
+  printf("BUY %d\n", qt);
 
+  cards = realloc(cards, sizeof(char*)*(qtCards + qt));
+
+  for (int i = 0; i < qt; i++) {
+    scanf("\n%m[^\n]", &cards[qtCards + i]);
+  }
+
+  qtCards += qt;
 }
 
-/*
-Rodrigo
-
-Função: retornar símbolo de carta passada como parâmetro
-
-Exemplo de entrada (parâmetro da função):
-A♥
-
-Exemplo de saída (retorno da função):
-♥
-
-Remover primeiro caractére da string (atenção para não alterar a string original)
-*/
 char* getCardSymbol(char* card) {
   char* symbol;
   int cardLength = (int)strlen(card);
   symbol = calloc(cardLength-1, sizeof(char));
   
   for (int i = 1; i < cardLength; i++) {
-      symbol[i-1] = card[i];
+    symbol[i-1] = card[i];
   }
   
   return symbol;
